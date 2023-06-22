6926873757773f8e4a62f18e757ed8ef25d5d83d
Sharat Gouda <sharathpatil222@gmail.com>
2023-06-23 01:21:07 +0530

    Solution

---
+++ Learning_2023/Day4/Day5/Question2.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    char str[] = "5278";
+    int i = 0;
+    int value = 0;
+
+    while (str[i]) {
+        if (isdigit(str[i])) {
+            value = value * 10 + (str[i] - '0');
+        }
+        i++;
+    }
+
+    printf("String: %s\n", str);
+    printf("Integer: %d\n", value);
+
+    return 0;
+}
